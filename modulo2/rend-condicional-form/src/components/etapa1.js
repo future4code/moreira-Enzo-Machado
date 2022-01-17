c41223bef4e357c7ed754eefa7e559e2011c8fce
MrNobary <enzogem0@gmail.com>
2022-01-17 17:53:06 -0300

    condicionais de pagina não funcionando

---
+++ modulo2/rend-condicional-form/src/components/etapa1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class etapa1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>ETAPA 1 - DADOS GERAIS</h3>
+        <input>1. Qual o seu nome?</input>
+      </div>
+    )
+
+  }
+}
---
+++ modulo2/rend-condicional-form/src/components/etapa3.js
@@ -0,0 +1,11 @@
+
+
+function etapa3() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+    }
+  
+export default etapa3;
