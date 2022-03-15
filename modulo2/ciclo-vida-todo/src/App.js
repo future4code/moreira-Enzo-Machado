c2ce28a30fcd9b0d208d12e8d2fa73ee0a41705b
MrNobary <enzogem0@gmail.com>
2022-03-15 02:04:43 -0300

    REC-TASK-COMPLETE

--- modulo2/ciclo-vida-todo/src/App.js
+++ modulo2/ciclo-vida-todo/src/App.js
@@ -2,113 +2,167 @@ import React from 'react'
 import styled from 'styled-components'
 import './styles.css'
 
-const TarefaList = styled.ul`
-  padding: 0;
-  width: 200px;
-`
-
-const Tarefa = styled.li`
-  text-align: left;
-  list-style-type: circle;
-  width: 320px;
-  padding: 10px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  border-bottom-right-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  margin: 10px;
-  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
-  background-color: ${({completa}) => (completa ? '#d7d7d7' : '#fff')};
-  /*                                          SE FOR           SE NÃO        */
-`
-
-const InputsContainer = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  gap: 10px;
-  display: flex;
-  align-items: center;
-
-  input {
-    width: 100%;
-    padding: 6px 20px;
-    margin: 8px 0;
-    box-sizing: border-box;
-    border: none;
-    border-radius: 10px;
-    background-color: orange;
-    color: black;
-  }
-
-  button {
-	display: inline-block;
-	position: relative;
-	transition-duration: .3s;
-	transition-property: transform;
-  height: 25px;
-  width: 80px;
-  background-color: black;
-  color: white;
-  border-radius: 15px 30px 30px 15px;
-  padding-left: 10px;
-
-  
-
-  &:before {
-    pointer-events: none;
-		position: absolute;
-		z-index: -1;
-		content: '';
-		top: calc(50% - 10px);
-		left: 0;
-		border-style: solid;
-		border-width: 10px 10px 10px 0;
-		border-color: transparent #ececec transparent transparent;
-		transition-duration: .3s;
-		transition-property: left;
-  }
-
-  &:hover {
-    transform: translateX(5px);
-  }
-
-  &:hover:before {
-    left: -(10px);
-  }
-  }
-`
-
-const AppScreen = styled.div`
-  font-family: sans-serif;
-  text-align: center;
+const AppView = styled.div`
+  background-color: #F8F5DF;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  height: 100vh;
-  border-radius: 30px;
-  background-color: #d7d7d7;
+  
+`
+
+const TaskContainer = styled.div`
+  height: 436px;
+  width: 958px;
+  background-color: #D7A2F6;
+  border: 5px solid black;
+  border-top:none;
+  border-bottom-left-radius: 35px;
+  border-bottom-right-radius: 35px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
+`
+
+const TaskList = styled.div`
+
+`
+
+const Tarefa = styled.li`
+  width: 414px;
+  text-align: left;
+  font-size: 20px;
+  list-style-type: none;
+  padding: 10px;
+  border-radius: 40px;
+  border: 5px solid black;
+  margin: 10px;
+  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
+  background-color: ${({completa}) => (completa ? 'rgba(0,0,0,0.5)' : '#D7A2F6')};
+
+`
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 35px;
+  background-color: #F8F5DF;
+  border: 5px solid black;
+  border-top-left-radius: 35px;
+  border-top-right-radius: 35px;
+  width: 958px; 
 
   h1 {
-    margin: 50px 0 5 0;
+    margin-right: 360px;
   }
 `
 
-class App extends React.Component {
-    state = {
-      tarefas: [
-        {
-          id: Date.now(),
-          texto: "Varrer a casa",
-          completa: false
-        }
-      ],
-      inputValue: '',
-      filtro: '',
+const Filtro = styled.div`
+  display: flex;
+  margin-right: 10px;
 
-      textoNovaTarefa: ''
+  select {
+    margin-top: 20px;
+    margin-bottom: 15px; 
+    border: 3px solid black;
+    border-radius: 35px;
+    background-color: #d7a2f6;
+  }
+`
 
+const Bolas = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 30px;
+`
+
+const Bola1 = styled.div`
+  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  background-color: #F98509;
+  border: 3px solid black;
+  margin-right: 5px;
+`
+
+const Bola2 = styled.div`
+  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  background-color: #F2E018;
+  border: 3px solid black;
+  margin-right: 5px;
+  margin-left: 5px;
+`
+
+const Bola3 = styled.div`
+  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  background-color: #FFFFFF;
+  border: 3px solid black;
+  margin-left: 5px;
+`
+
+const AddContainer = styled.div`
+  background-color: #F98509;
+  width: 958px;
+  height: 150px;
+  border: 5px solid black;
+  border-top:none;
+  border-bottom-left-radius: 35px;
+  border-bottom-right-radius: 35px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const AddBody = styled.div`
+  display: flex;
+  align-items: center;
+
+    input {
+      border: 5px solid black;
+      background-color: #F98509;
+      font-size: 25px;
+      border-radius: 35px;
+      margin-left: -30px;
+      margin-right: 30px; 
+      padding: 7px 10px 14px;
     }
+
+    button {
+      font-size: 25px;
+      border-radius: 35px;
+      border: 5px solid black;
+      color: #F8F5DF;
+      background-color: black;
+
+      :hover {
+        background-color: #F98509;
+        color: black;
+      }
+    }
+`
+
+class App extends React.Component {
+  state = {
+    tarefas : [
+      {
+        id: Date.now(),
+        texto: "Limpar a Casa",
+        completa: false
+      }
+    ],
+    
+    inputValue: '',
+    filtro: '',
+
+    textoNovaTarefa: ''
+  }
 
   componentDidUpdate() {
     localStorage.setItem("tarefas", JSON.stringify(this.state.tarefas))
@@ -119,7 +173,7 @@ class App extends React.Component {
     this.setState({
       tarefas: tarefasCriadas
     })
-  };
+  }
 
   onChangeInput = (event) => {
     this.setState({inputValue: event.target.value});
@@ -135,14 +189,14 @@ class App extends React.Component {
     const copiaDasTarefas = [...this.state.tarefas, novaTarefa]
 
     this.setState({tarefas: copiaDasTarefas})
-
+    
   }
 
   selectTarefa = (id) => {
     const novasTarefas = this.state.tarefas.map((status) => {
       if (id === status.id) {
         const estadoNovo = {
-          ...status,
+          ...status, 
           completa: !status.completa
         }
         return estadoNovo
@@ -166,41 +220,58 @@ class App extends React.Component {
           return !tarefa.completa
         case 'completas':
           return tarefa.completa
-        default:
+        default: 
           return true
       }
     })
 
     return (
-      <AppScreen>
-        <h1>Lista de tarefas</h1>
-        <InputsContainer>
-          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
-          <button onClick={this.criaTarefa}>Add</button>
-        </InputsContainer>
-        <br/>
+      <AppView>
+          <Header>
+            <Bolas>
+              <Bola1/>
+              <Bola2/>
+              <Bola3/>
+            </Bolas>
 
-        <InputsContainer>
-          <label>Filtro</label>
-          <select value={this.state.filter} onChange={this.onChangeFilter}>
-            <option value="">Nenhum</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="completas">Completas</option>
-          </select>
-        </InputsContainer>
-        <TarefaList>
-          {listaFiltrada.map(tarefa => {
-            return (
-              <Tarefa
-                completa={tarefa.completa}
-                onClick={() => this.selectTarefa(tarefa.id)}
-              >
-                {tarefa.texto}
-              </Tarefa>
-            )
-          })}
-        </TarefaList>
-      </AppScreen>
+            <h1>Lista de Tarefas</h1>
+          </Header>
+        <TaskContainer>
+          <Filtro>
+                <select value={this.state.filter} onChange={this.onChangeFilter}>
+                  <option value="">Nenhum</option>
+                  <option value="pendentes">Pendentes</option>
+                  <option value="completas">Completas</option>
+                </select>
+          </Filtro>
+
+          <TaskList>
+            {listaFiltrada.map(tarefa => {
+              return(
+                <Tarefa
+                  completa={tarefa.completa}
+                  onClick={() => this.selectTarefa(tarefa.id)}>
+                    {tarefa.texto}
+                  </Tarefa>
+              )
+            })}
+          </TaskList>
+        </TaskContainer>
+        <Header>
+          <Bolas>
+            <Bola1/>
+            <Bola2/>
+            <Bola3/>
+          </Bolas>
+            <h1>Adicionar Tarefa</h1>
+          </Header>
+        <AddContainer>
+          <AddBody>
+            <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+            <button onClick={this.criaTarefa}>Submit</button>
+          </AddBody>
+        </AddContainer>
+      </AppView>
     )
   }
 }
