3f9a63876063441b6e3a51d4b045216f07c8a228
MrNobary <enzogem0@gmail.com>
2022-03-24 07:47:27 -0300

    IntroTS-Completo

--- modulo4/intro-typescript/src/Exercicio5.ts
+++ modulo4/intro-typescript/src/Exercicio5.ts
@@ -1,2 +1,21 @@
 
 
+function checaRenovacaoRg():boolean {
+
+    let anoAtual:number = Number(process.argv[2])
+    let anoNascimento:number = Number(process.argv[3])
+    let anoEmissao:number = Number(process.argv[4])
+
+    let idade = anoAtual - anoNascimento
+    let tempoCarteira = anoAtual - anoEmissao
+
+    let cond1:boolean = idade <= 20 && tempoCarteira >=5
+    let cond2:boolean = idade > 20 && idade <= 50 && tempoCarteira >=10
+    let cond3:boolean = idade > 50 && tempoCarteira >=15
+
+    return(cond1||cond2||cond3)
+
+    
+}
+
+console.log(checaRenovacaoRg);
